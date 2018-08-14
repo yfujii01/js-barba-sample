74a97ec2e2d6d4f42bf93d6b882ca5b64cae5871
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-08-14 14:50:06 +0900

    リンク押下時に隠す感じ

--- module/script.js
+++ module/script.js
@@ -1,46 +1,43 @@
 //ページ遷移設定(pjax)
-(function () {
-    (function () {
-        // const shutterA = document.querySelector('.shutter-a');
-        // const shutterB = document.querySelector('.shutter-b');
+const ShutterAnimation = Barba.BaseTransition.extend({
+    // コンストラクタ
+    start: start_fnc,
+    // アニメーションさせる処理
+    anime: anime_fnc,
+    // アニメーションの終了を示すためにthis.done()を呼ぶことが必須
+    finish: finish_fnc
+});
 
-        const ShutterAnimation = Barba.BaseTransition.extend({
+function start_fnc() {
+    this.anime(400)
+        .then(this.newContainerLoading)
+        .then(this.finish.bind(this));
+}
 
-            // Barba.jsで定義されている。コンストラクタと考えてよいそうです。
-            start: function () {
-                this.shutter(400)
-                    .then(this.newContainerLoading)
-                    .then(this.finish.bind(this));
-            },
+function anime_fnc(timer) {
+    return new Promise(anime_do);
 
-            // シャッターが閉まるようなアニメーションをさせる処理
-            shutter: function (timer) {
-                return new Promise(function (resolve) {
-                    // クラスを付け替え、移動させる
-                    document.querySelector('.shutter-a').classList.toggle('moved');
-                    document.querySelector('.shutter-b').classList.toggle('moved');
+    function anime_do(resolve) {
+        // クラスを付け替え、移動させる
+        document.querySelector('.shutter-a').classList.toggle('moved');
+        document.querySelector('.shutter-b').classList.toggle('moved');
 
-                    // 画面が黄色く埋まるまで（400ms）待つ
-                    setTimeout(function () {
-                        resolve();
-                    }, timer);
+        setTimeout(function () {
+            resolve();
+            document.querySelector('.shutter-a').classList.toggle('moved');
+            document.querySelector('.shutter-b').classList.toggle('moved');
+        }, timer);
+    }
+}
+function finish_fnc() {
+    // document.body.scrollTop = 0;
+    this.done();
+}
 
-                });
-            },
 
-            // アニメーションの終了を示すためにthis.done()を呼ぶことが必須
-            finish: function () {
-                document.body.scrollTop = 0;
-                this.done();
-            }
+Barba.Pjax.getTransition = function () {
+    return ShutterAnimation;
+};
 
-        });
-
-        Barba.Pjax.getTransition = function () {
-            return ShutterAnimation;
-        };
-    })();
-
-    // Barba.js 実行
-    Barba.Pjax.start();
-})();
+// Barba.js 実行
+Barba.Pjax.start();
